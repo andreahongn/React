2dddf100eac28f3e0d971c1e612baa9815786460
andreahongn <andreahongn@gmail.com>
2022-09-30 13:01:48 -0300

    se agrega header

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="App">
+      <h1>hola mundo</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // Esto es código JSX (html + JS)
+  //  {/* strict mode,equivalente a una funcion que cuando declaro que esta activado, todo lo encerrado tiene modo estricto*/}
+  // {/* en JS se pondria "use strict" */}
   <React.StrictMode>
+    {/* equivalente a: const app=()=>{pasan un monton de coss que terminan renderiazando la pantalla} */}
     <App />
   </React.StrictMode>
 );
